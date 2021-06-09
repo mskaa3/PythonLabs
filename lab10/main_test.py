cc81d597e616543c50b702e44e9719319c8fd1f8
mskaa3 <j.moskaaa@gmail.com>
2021-06-09 02:01:38 +0200

    doneeeee

--- lab10/main_test.py
+++ lab10/main_test.py
@@ -5,8 +5,6 @@ httpMethod = ("PUT", "OPTIONS", "CONNECT", "GET", "HEAD", "POST", "DELETE", "PAT
 code = ("HTTP/1.1", "HTTP/1.0", "HTTP/2.0")
 
 
-class noneException(Exception):
-    pass
 
 
 class TypeError(Exception):
@@ -28,21 +26,21 @@ class ValueError(Exception):
 class HTTP_request:
     def __init__(self, path):
         my_list = path.split(' ')
-        if len(my_list) != 3:
-            raise noneException
+        print(my_list)
+
+
+        if str(my_list[0]) in httpMethod:
+            self.request = my_list[0]
         else:
-            if str(my_list[0]) in httpMethod:
-                self.request = my_list[0]
-            else:
-                raise BadRequestTypeError
-            if '/' == my_list[1][0]:
-                self.path = my_list[1]
-            else:
-                raise ValueError
-            if my_list[2] in code:
-                self.protocol = my_list[2]
-            else:
-                raise BadHTTPVersion
+            raise BadRequestTypeError
+        if '/' == my_list[1][0]:
+            self.path = my_list[1]
+        else:
+            raise ValueError
+        if my_list[2] in code:
+            self.protocol = my_list[2]
+        else:
+            raise BadHTTPVersion
 
     def __str__(self):
         return f'request type: {self.request},path {self.path}, http:{self.protocol}'
@@ -51,10 +49,11 @@ class HTTP_request:
 def reqstr2obj(request_string):
     if type(request_string) != str:
         raise TypeError
-    try:
-        return HTTP_request(request_string)
-    except noneException:
+    if len(request_string.split(' ')) !=3:
         return None
+    else:
+        return HTTP_request(request_string)
+
 
 # 4. It doesnt have to return anything, its just have to raise a proper exception, because
 # test is about the exception itself
@@ -66,26 +65,24 @@ def test_reqstr2obj1():
 
 
 def test_reqstr2obj2():
-    assert type(HTTP_request('GET / HTTP/1.1')) == HTTP_request
+    assert type(reqstr2obj('GET / HTTP/1.1')) == HTTP_request
 
 
 def test_reqstr2obj3():
-    assert HTTP_request('GET / HTTP/1.1').request == 'GET' and \
-           HTTP_request('GET / HTTP/1.1').path == '/' and\
-           HTTP_request('GET / HTTP/1.1').protocol == 'HTTP/1.1'
+    arg=HTTP_request('GET / HTTP/1.1')
+    assert arg.request == 'GET' and  arg.path == '/' and\
+           arg.protocol == 'HTTP/1.1'
 
 
 def test_reqstr2obj4():
     line = 'CONNECT /blabla HTTP/1.1'
     new_list = line.split(' ')
     fun = reqstr2obj(line)
-    assert fun.request == new_list[0] \
-        and fun.path == new_list[1] \
-        and fun.protocol == new_list[2]
+    assert fun.request == new_list[0] and fun.path == new_list[1] and fun.protocol == new_list[2]
 
 
 def test_reqstr2obj5():
-    assert reqstr2obj('GET  / HTTP/1.1"') is None
+    assert reqstr2obj('GET / HTTP/1.1') == None
 
 
 def test_reqstr2obj6():
@@ -104,4 +101,4 @@ def test_reqstr2obj8():
 
 
 if __name__ == '__main__':
-    print(HTTP_request('GET /new HTTP/1.1').__str__())
+    HTTP_request('GET/ HTTP/1.1')
